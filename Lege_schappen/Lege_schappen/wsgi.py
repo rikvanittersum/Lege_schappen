28df3874bcf48f4002217cca5b99af67af7f30f7
rvittersum <rvittersum@sogyo.nl>
2020-07-28 11:55:06 +0200

    init

---
+++ Lege_schappen/Lege_schappen/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Lege_schappen project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lege_schappen.settings')
+
+application = get_wsgi_application()
