2f0319db6b2a3eecd194bfd4610b1de5b413e830
rvittersum <rvittersum@sogyo.nl>
2020-08-05 13:37:13 +0200

    Succesvol tabel met vue js

--- src/supermarkten/views.py
+++ src/supermarkten/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Supermarkt
+import json
+from django.core import serializers
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, 'home.html', {})
 
 def supermarkten_view(request, *args, **kwargs):
-    supermarkten = Supermarkt.objects.all()
-    supermarkten[0].voeg_uitverkocht_product_toe("henk")
-    context = {"supermarkten" : supermarkten}
+    data = serializers.serialize('python', Supermarkt.objects.all())
+    objecten = [d['fields'] for d in data]
+    joe = json.dumps(objecten)
+    context = {"supermarkten": joe}
     return render(request, 'supermarkten.html', context)
